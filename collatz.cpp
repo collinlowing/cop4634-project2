990e16ee120e4308f17d9c9ff24e9c645faddeb5
CharlesTravis80 <59668201+CharlesTravis80@users.noreply.github.com>
2020-10-19 02:40:14 -0500

    Update collatz.cpp

--- collatz.cpp
+++ collatz.cpp
@@ -1,8 +1,46 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<cstring>
+#include <pthread.h>
+#include <cstdlib>
+int collatzFunction(int);
 
-int main()
+int counter = 12;
+
+int main(int argCount, char** argArr)
 {
-  std::cout << "Test makefile";
+    std::string argCommandRangeMax =            argArr[1];
+    std::string argCommandThreadCount =         argArr[2];
 
-  return 0;
+    int rangeMax =                              stoi(argCommandRangeMax);
+    int threadCount =                           stoi(argCommandThreadCount);
+
+
+    collatzFunction(counter);
+
+    //TEST OUTPUT
+    std::cout << "collatz number range 2 - " << rangeMax << std::endl;
+    std::cout << "active thread count " << threadCount << std::endl;
+    std::endl;
+
+    return 0;
+}
+
+int collatzFunction(int counter)
+{
+    int stoppingTime = 0;
+
+    while(counter != 1)
+    {
+        stoppingTime++;
+        if(counter % 2 == 0)
+        {
+            counter = counter/2;
+        }
+        else
+            counter = counter * 3 + 1;
+        }
+    //TEST OUTPUT BELOW
+    std::cout << counter << std::endl;
+    std::cout << stoppingTime << std::endl;
 }
