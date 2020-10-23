5b17184ff8560ea8f75f87e3df42d60ea2ed3e28
Collin Lowing <cel19@students.uwf.edu>
2020-10-23 16:38:14 -0500

    adds TODO list to codes, adds gaurds for commandline arguments

--- collatz.cpp
+++ collatz.cpp
@@ -1,14 +1,30 @@
-#include<iostream>
-#include<string>
-#include<cstring>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <pthread.h>
 #include <cstdlib>
-int collatzFunction(int);
+
+// TODO: Record Runtime
+// TODO: Create Threads
 
 int counter = 12;
 
+int collatzFunction(int);
+
+
 int main(int argCount, char** argArr)
 {
+  std::cout << "test" << std::endl;
+    // Guard for no commadline arguments
+    if(argCount <= 2)
+    {
+      std::cout << "FATAL ERROR: Not enough commadline arguments, please try again." << std::endl;
+
+      return 1;
+    }
+std::cout << "test" << std::endl;
+
+
     std::string argCommandRangeMax =            argArr[1];
     std::string argCommandThreadCount =         argArr[2];
 
@@ -19,10 +35,11 @@ int main(int argCount, char** argArr)
     collatzFunction(counter);
 
     //TEST OUTPUT
-    std::cout << "collatz number range 2 - " << rangeMax << std::endl;
-    std::cout << "active thread count " << threadCount << std::endl;
-    std::endl;
+    std::cout << "collatz number range 2 - " << rangeMax 	<< std::endl;
+    std::cout << "active thread count " << threadCount 		<< std::endl
+    								<< std::endl;
 
+    // TODO: Print out runtime in stderr output
     return 0;
 }
 
@@ -39,8 +56,13 @@ int collatzFunction(int counter)
         }
         else
             counter = counter * 3 + 1;
-        }
+     }
     //TEST OUTPUT BELOW
     std::cout << counter << std::endl;
     std::cout << stoppingTime << std::endl;
+
+    // TODO: Output results from Collatz Sequence
+    // k + ", " + stoppingTime
+
+    return 0;
 }
